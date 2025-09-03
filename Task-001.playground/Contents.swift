0361425ab49f786b81abb4b9bd5181af7b4ea48b
Sergei <sergzhur.career@gmail.com>
2025-09-03 12:24:12 -0500

    Task 001: solution (uncommented)

---
+++ Task-001.playground/Contents.swift
@@ -0,0 +1,30 @@
+// Работа с базовыми типами
+
+
+// Д/з 1
+
+
+//Int
+//UInt
+//Float
+//Double
+
+
+//Int
+print("Минимальное значение Int:", Int.min)
+print("Максимальное значение Int:", Int.max)
+
+
+//UInt
+print("Минимальное значение UInt:", UInt.min)
+print("Максимальное значение UInt:", UInt.max)
+
+
+//Float
+print("Минимальное значение Float:", -Float.greatestFiniteMagnitude)
+print("Максимальное значение Float:", Float.greatestFiniteMagnitude)
+
+
+//Double
+print("Минимальное значение Double:", -Double.greatestFiniteMagnitude)
+print("Максимальное значение Double:", Double.greatestFiniteMagnitude)
